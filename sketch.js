a3ef860b278515c22cd19fdbba52542520ab505a
Kyle Arquilla <kaarquilla@gmail.com>
2023-10-15 19:49:05 -0400

    game over functionality, added game class

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let mapW = 2000;
 let mapH = 1000;
-let lvl1ShotCount = 5;
 
-let gameState = 'start'; // start, title, run, stage clear/ game over.
-let levelNum = 0;
+
+ 
+
 
 function preload()
 {
@@ -15,21 +15,23 @@ function setup()
     new Canvas(mapW,mapH);
     frameRate(60);
     world.gravity.y = 2;
-    level1 = new level(lvl1ShotCount);
+    game = new Game;
+    level1 = new level(game.lvl1ShotCount);
     shot = new Shot;
     //temp run for debug
-    gameState = 'run';
+    game.gameState = game.run;
     // straight to level 1 for debug nomrally after title
-    levelNum++;
+    game.levelNum++;
+    game.currLvlObj = level1;
 }
 
 function draw()
 {
     clear();
-    if (gameState == 'run')
+    if (game.gameState == game.run)
     {   
         background(100,100,100);
-        switch(levelNum)
+        switch(game.levelNum)
         {
             case 1:
             {
@@ -39,6 +41,7 @@ function draw()
         }
         
     }
+    game.game_over()
     //console.log(level1.platforms)
 }
 
@@ -48,12 +51,20 @@ function Level1()
     //setup
     if (level1.isSetup == false)
     {
-        level1.create_platform(1000,50,100)
-        level1.create_platform(1500,75,200)
+        level1.create_platform( 900, 30, 230)
+        level1.create_platform(1000, 35, 260)
+        level1.create_platform(1100, 40, 290)
+        level1.create_platform(1200, 45, 320)
+        level1.create_platform(1300, 50, 350)
+        level1.create_platform(1400, 55, 380)
+        level1.create_platform(1500, 60, 410)
         level1.isSetup = true;
     }
     level1.target_is_destroyed();
     level1.shooting();
+    level1.UI();
+    level1.isGameOver();
+
 }
 
 class Shot
@@ -66,15 +77,16 @@ class Shot
         this.originY = mapH - 75;
         this.d = 30;
         this.power = 0;
-        this.powerMultiplier = 5;
+        this.powerMultiplier = 8;
         this.angle = 0;
+        this.uiElems = new Group();
         this.sprite = new Sprite(this.originX,this.originY,this.d,10,"k");
-        this.sightline = new Sprite(this.originX,this.originY,100,5,"n");
+        this.sightline = new this.uiElems.Sprite(this.originX,this.originY,100,5,"n");
         this.sightline.img = sightlineImg;
         this.sightline.offset.x = -100;
         this.sprite.overlaps(this.sightline);
-        this.barsprite = new Sprite(this.originX - 200,this.originY,100,25,'n');
-        this.barmaxsprite = new Sprite(this.originX - 100,this.originY,5,25,'n')
+        this.barsprite = new this.uiElems.Sprite(this.originX - 200,this.originY,100,25,'n');
+        this.barmaxsprite = new this.uiElems.Sprite(this.originX - 100,this.originY,5,25,'n');
         this.r = 0
         this.g = 200
 
@@ -148,19 +160,22 @@ class level
         this.shotCount = shotCnt;
         this.isSetup = false;
         this.platforms = new Group();
+        this.platforms.mass = 30;
         this.targets = new Group();
         this.targets.rotationalDrag = 5;
         this.floor = new Sprite(mapW/2 + 200, mapH + 5, mapW + 500, 5, 's');
         this.leftbound = new Sprite(-50, mapH/2,5,mapH,'s');
         this.rightbound = new Sprite(mapW + 450, mapH/2,5,mapH,'s');
+        this.shotCountSprite = new Sprite(250,250, 20, 20, 'n');
     }
+
     // creates target on top of platform automatically
     // use to create map by placing x and size of platform
     create_platform(x,w,h)
     {
         var y = this.find_plat_bottom(h);
         new this.platforms.Sprite(x,y,w,h);
-        this.create_target(x,(y - (h/2)) - ((w)/2),w-10);
+        this.create_target(x,(y - (h/2)) - ((w)/2),w+10);
     }
 
     create_target(x,y,d)
@@ -184,32 +199,34 @@ class level
         }
         return false;
     }
+
     // Removes target when collides with 
     // floor after 300ms delay
     target_is_destroyed()
     {
         for (var i = 0; i < this.targets.length; i++)
         {
-            if (this.targets[i].collides(this.floor))
+            if (this.targets[i].overlaps(shot.sprite) || this.targets[i].collides(this.floor))
             {
                 this.targets[i].speed = 0;
                 let destroyed = this.targets[i];
-                setTimeout(() => {destroyed.remove();}, 300);  
+                destroyed.remove()
+                //setTimeout(() => {destroyed.remove();}, 300);  
             }
         }
     }
 
     isGameOver()
     {
-        if (self.shotCount == 0)
+        if (this.shotCount == 0)
         {
-
+            game.gameState = game.gameOver
         }
     }
 
     isComplete()
     {
-        if (this.targets.length == 0)
+        if (this.targets.length <= 0)
         {
             return true;
         }
@@ -242,6 +259,7 @@ class level
         {
             if ( shot.sprite.speed < .05 && shot.sprite.speed > 0)
             {
+                shot.shooting = false;
                 setTimeout(() => {
                 shot.sprite.remove();
                 shot.sightline.remove();
@@ -251,6 +269,39 @@ class level
             }
         }
     }
+
+    UI()
+    {
+        this.shotCountSprite.text = this.shotCount
+    }
+
+}
+
+class Game
+{
+    constructor()
+    {
+        this.lvl1ShotCount = 2;
+        this.gameState = 0; // start, title, run, stageclear/ gameover.
+
+        this.levelNum = 0;
+        // Enum for game states
+        this.title = 1;
+        this.run = 2;
+        this.stageClear = 3;
+        this.gameOver = 4;
+        this.load = 5;
+        this.currLvlObj;
+    }
+
+    game_over()
+    {
+        if (this.gameState == this.gameOver)
+        {
+            this.currLvlObj.uiElems.remove();
+        }
+    }
+
 }
 
 //generic level class
